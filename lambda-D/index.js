b65f3d88d213df17ff84fce36109eb97dcdcc83e
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-07 20:01:16 +0530

    Lambda D Changed

--- lambda-D/index.js
+++ lambda-D/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-D!'),
+        body: JSON.stringify('Hello from Lambda-D! - Updated Version V4'),
     };
     return response;
 };
