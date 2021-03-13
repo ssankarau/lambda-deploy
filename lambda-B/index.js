e4bcf796162e890c6424ee108eb8a78ad3623304
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-13 23:36:51 +0530

    Lambda Version Update

--- lambda-B/index.js
+++ lambda-B/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-B!-v10'),
+        body: JSON.stringify('Hello from Lambda-B!-v11'),
     };
     return response;
 };
