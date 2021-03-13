6fa1202b72e26a31c13d41d280596c71730b86c1
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-13 12:44:37 +0530

    Resetting

--- lambda-B/index.js
+++ lambda-B/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-B!'),
+        body: JSON.stringify('Hello from Lambda-B!-v2'),
     };
     return response;
 };
