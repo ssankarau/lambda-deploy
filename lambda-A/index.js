307cdfdd3eca927f3d320ac669bdc878c35f19a3
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-14 10:51:52 +0530

    Function Change

--- lambda-A/index.js
+++ lambda-A/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-A!-Static Name-V3'),
+        body: JSON.stringify('Hello from Lambda-A!-Static Name-V4'),
     };
     return response;
 };
