262552676151f35cd19b0503f0a4abc2585f8959
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-14 10:45:36 +0530

    Lambda New Function

--- lambda-A/index.js
+++ lambda-A/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-A!-Static Name-V2'),
+        body: JSON.stringify('Hello from Lambda-A!-Static Name-V3'),
     };
     return response;
 };
