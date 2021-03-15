989a56a4cec666e70a92d514e1446bdeb32dc507
Sankaranarayanan S <ssankarmca37@gmail.com>
2021-03-15 15:11:17 +0530

    Version2 A

--- lambda-A/index.js
+++ lambda-A/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda-A- Name Updated!'),
+        body: JSON.stringify('Hello from Lambda-A'),
     };
     return response;
 };
